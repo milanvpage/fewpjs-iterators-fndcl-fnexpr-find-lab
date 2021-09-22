dca9465fedd000ef60907f7f63a1c734a5caf7a0
Milan <milanvpage@gmail.com>
2021-09-22 15:33:33 -0700

    commit tree 4306764ada1b1b33ab434fc35220da4e700f527a

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+]
 
+function superbowlWin(record) {
+    let found = record.find(elem => elem.result === "W")
+    //return !!found? found.year:undefined;
+    if (!!found){
+        return found.year
+    }
+    else {
+        return undefined
+    }
+    
+}
